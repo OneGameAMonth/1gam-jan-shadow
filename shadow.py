a263464dba7a2d9e4375af963509d4ff05c23035
Alexandru Pana <alex.pana@vertexarmy.org>
2013-01-08 23:44:10 +0200

    Replaced bash script with python script, refactored the shadow test

---
+++ shadow.py
@@ -0,0 +1,3 @@
+from tests import TestShadow
+
+TestShadow.run()
--- tests/TestShadow.py
+++ tests/TestShadow.py
@@ -1,32 +1,28 @@
 import sys
-import os.path
-oldpath = sys.path[:]
-sys.path.append(os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
-
 import pygame
 from pygame.locals import *
-from pygame.rect import *
 
 from game.Renderer import Renderer
-from game.Light import *
 from game import Shadow
 from game.Vector import Vec2
 from game.components.Transform import *
 
-
-pygame.init()
-pygame.display.set_caption("Shadow")
-
 renderer = Renderer()
-renderer.init((800, 600))
-renderer.antialiasing = False
-renderer.clearColor = Color(31, 32, 34)
-renderer.color = Color(200, 100, 0)
-
 Vertices = [ (0, 50), (-50, -30), (50, -30) ]
- 
 mousePosition = (0, 0)
-pygame.mouse.set_visible(False)
+
+def initialize():
+    global renderer
+    pygame.init()
+    
+    pygame.display.set_caption("Shadow")
+    pygame.mouse.set_visible(False)
+    
+    renderer = Renderer()
+    renderer.init((800, 600))
+    renderer.antialiasing = False
+    renderer.clearColor = Color(31, 32, 34)
+    renderer.color = Color(200, 100, 0)
 
 def renderLightSource():
     renderer.setTransform(NullTransform)
@@ -35,7 +31,7 @@ def renderLightSource():
     
 def renderShadows():
     renderer.color = Color(20, 21, 22)
-    shadow = Shadow._extrudeVertices( Vertices, Vec2.fromTuple(mousePosition) - Vec2(400, 300))
+    shadow = Shadow._extrudeVertices(Vertices, Vec2.fromTuple(mousePosition) - Vec2(400, 300))
     renderer.renderFilledPolygon(shadow)
     
 def renderMesh():
@@ -44,22 +40,31 @@ def renderMesh():
     renderer.renderFilledPolygon(Vertices)
     renderer.antialiasing = False
 
-    
-while(True):
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == MOUSEMOTION:
-            mousePosition = event.pos
-
+def render():
     renderer.begin()
-    
     renderLightSource()
     renderer.setTransform(Transform(translation=(400, 300)))
     renderShadows()    
     renderMesh()
-
     renderer.end()
+    
+def handleEvent(event):
+    global mousePosition
+    if event.type == QUIT:
+        pygame.quit()
+        sys.exit()
+    if event.type == MOUSEMOTION:
+        mousePosition = event.pos
+    
+def update():
+    for event in pygame.event.get():
+        handleEvent(event)
 
-sys.path = oldpath
+def run():
+    initialize()
+    while(True):
+        update()
+        render()
+
+if __name__ == "__main__":
+    run()
